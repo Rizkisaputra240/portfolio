37b4d2f03cdd4f24e3cd77fc870eaa624de46090
DWI <dwi@gmail.com>
2026-08-24 23:52:27 +0700

    memperbaiki error nama file import pdf di Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="kontak saya" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./CV-Rizki-Update.pdf")}
+                    download="CV-Rizki-Update.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download CV" />
